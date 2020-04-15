5d8952e96f7d01fb7d7171003dbce55420d5a250
Nicolai Bach Woller <woller@traels.it>
2020-04-15 10:33:19 +0200

    New feature. Generate a new gem.

---
+++ lib/simplecov/post/formatter.rb
@@ -0,0 +1,10 @@
+require "simplecov/post/formatter/version"
+
+module Simplecov
+  module Post
+    module Formatter
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/simplecov/post/formatter/version.rb
@@ -0,0 +1,7 @@
+module Simplecov
+  module Post
+    module Formatter
+      VERSION = "0.1.0"
+    end
+  end
+end
