a9570dfa2c3ffafc6d01f687a5a674975d554a92
Nicolai Bach Woller <woller@traels.it>
2020-04-16 14:57:55 +0200

    Chore. Fix simplecov coverage not working.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,25 +1,17 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "simplecov"
+SimpleCov.start
 require "simplecov_badger"
 require "minitest/autorun"
 require 'minitest/unit'
 require "mocha/minitest"
-require "simplecov"
-
-SimpleCov.start do
-  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
-    [
-      SimpleCov::Formatter::HTMLFormatter,
-      SimplecovBadger::Formatter
-    ]
-  )
-end
 
 SimplecovBadger.configure do |config|
   config.post_url = "www.test-post.com"
   config.repo_url = "www.test-repo.com"
   config.run_when = -> { false }
 end
-
 
 def mock_result(total_cov)
   stub(source_files: stub(covered_percent: total_cov))
